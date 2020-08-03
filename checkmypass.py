7292f386a6f6a779ddf2ee753f9c9bd9493f9b0b
Ryley Johnson <ryleyjohnson@Ryleys-MacBook-Pro.local>
2020-08-03 19:28:46 -0400

    working program

--- checkmypass.py
+++ checkmypass.py
@@ -1,5 +1,6 @@
 import requests
 import hashlib
+import sys
 
 
 
@@ -13,16 +14,33 @@ def request_api_data(query_char):
 
 
 def get_password_leaks_count(hashes, hashes_to_check):
-	hashes = (line.split(":") for line in hashes.text)
+	hashes = (line.split(":") for line in hashes.text.splitlines())
+	for h, count in hashes:
+		if h == hashes_to_check:
+			return count
+	return 0
+
 
 def pwned_api_check(password):
 	# check password if it exists in API response
 	sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
 	first5_char, tail = sha1password[:5], sha1password[5:]
 	response = request_api_data(first5_char)
-	print(response)
 	return get_password_leaks_count(response,tail)
 
 	
-pwned_api_check('1234')
+def main(args):
+	for password in args:
+		count = pwned_api_check(password)
+		if count:
+			print(f"{password} was found {count} times. You should probably change your password!")
+		else:
+			print(f"{password} was not found. Carry on!")
+		return 'Done!'	
+
+
+
+
+if __name__ == '__main__':
+	sys.exit(main(sys.argv[1:]))
 
