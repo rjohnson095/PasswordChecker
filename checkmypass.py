0964bc0b0d1e9c51494cbd5fe4c16b0a9c73c829
Ryley Johnson <ryleyjohnson@Ryleys-MacBook-Pro.local>
2020-08-03 19:02:45 -0400

    adding hash checker

--- checkmypass.py
+++ checkmypass.py
@@ -6,23 +6,23 @@ import hashlib
 def request_api_data(query_char):
 	# k-anonymity
 	url = "https://api.pwnedpasswords.com/range/" + query_char
-
 	res = requests.get(url)
 	if res.status_code != 200:
 		raise RunTimeError(f"Error fetching {res.status_code}, check the API and try again.")
-		return res 
-	print(res)
+	return res
 
 
-def read_res(response):
-	print(response.text)
+def get_password_leaks_count(hashes, hashes_to_check):
+	hashes = (line.split(":") for line in hashes.text)
 
 def pwned_api_check(password):
 	# check password if it exists in API response
 	sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
 	first5_char, tail = sha1password[:5], sha1password[5:]
 	response = request_api_data(first5_char)
-	return read_res(response)
+	print(response)
+	return get_password_leaks_count(response,tail)
 
+	
 pwned_api_check('1234')
 
